6c1b2896e60ab1bf9d2457c11475166f8b46fa38
Nadia <ncampowoytuk@gmail.com>
2017-10-24 13:10:10 +0200

    view index.html

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname);
-app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
