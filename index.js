a1f82f4f0b891f85525938b94b18120ac75a7365
Nadia <ncampowoytuk@gmail.com>
2017-10-24 12:45:19 +0200

    index test

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/images'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname);
+app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('index');
